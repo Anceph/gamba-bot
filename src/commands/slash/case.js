17dd5eaab06538ea10418193702187fff5f86f21
Anceph <yucehasan31@gmail.com>
2024-06-19 09:36:44 +0300

    typo

--- src/commands/slash/case.js
+++ src/commands/slash/case.js
@@ -70,193 +70,194 @@ export default {
         } else {
             keyIcon = skinsData.shop.skins['Operation Breakout Case Key'][0].icon
 
-        userData.cooldowns.command = Date.now() + 3.5 * 1000
-        await userData.save()
+            userData.cooldowns.command = Date.now() + 3.5 * 1000
+            await userData.save()
 
-        const errorEmbed = new EmbedBuilder()
-            .setTitle('Error')
-            .setColor('Red')
-            .setDescription(`You need a **${skinsData[caseName]['name']} Key** to open this case!`)
-            .setThumbnail(`${keyIcon}`)
+            const errorEmbed = new EmbedBuilder()
+                .setTitle('Error')
+                .setColor('Red')
+                .setDescription(`You need a **${skinsData[caseName]['name']} Key** to open this case!`)
+                .setThumbnail(`${keyIcon}`)
 
-        const infoEmbed = new EmbedBuilder()
-            .setTitle('Info')
-            .setColor(0xf0b129)
+            const infoEmbed = new EmbedBuilder()
+                .setTitle('Info')
+                .setColor(0xf0b129)
 
-        if (!dbData) {
-            inventory.create({
-                user_id: interaction.user.id
-            })
-            infoEmbed.setDescription(`You didn't have an inventory, so I created one! Try opening a case again.`)
-            return interaction.reply({ embeds: [infoEmbed] })
-        } else {
-            if (dbData.inventory == null) return interaction.reply({ embeds: [errorEmbed] })
-            let keyName
-            if (skinsData[caseName]['name'] == 'Operation Breakout Weapon Case') {
-                keyName = 'Operation Breakout Case Key'
+            if (!dbData) {
+                inventory.create({
+                    user_id: interaction.user.id
+                })
+                infoEmbed.setDescription(`You didn't have an inventory, so I created one! Try opening a case again.`)
+                return interaction.reply({ embeds: [infoEmbed] })
             } else {
-                keyName = `${skinsData[caseName]['name']} Key`
+                if (dbData.inventory == null) return interaction.reply({ embeds: [errorEmbed] })
+                let keyName
+                if (skinsData[caseName]['name'] == 'Operation Breakout Weapon Case') {
+                    keyName = 'Operation Breakout Case Key'
+                } else {
+                    keyName = `${skinsData[caseName]['name']} Key`
+                }
+                const foundKey = dbData.inventory.some((item) => item.skin === keyName)
+                if (!foundKey) return interaction.editReply({ embeds: [errorEmbed] })
+                await dbData.save()
             }
-            const foundKey = dbData.inventory.some((item) => item.skin === keyName)
-            if (!foundKey) return interaction.editReply({ embeds: [errorEmbed] })
-            await dbData.save()
-        }
 
-        const opening = new EmbedBuilder()
-            .setTitle(`${skinsData[caseName].name}`)
-            .setDescription(`Opening the case...`)
-            .setThumbnail(`${skinsData[caseName].icon}`)
+            const opening = new EmbedBuilder()
+                .setTitle(`${skinsData[caseName].name}`)
+                .setDescription(`Opening the case...`)
+                .setThumbnail(`${skinsData[caseName].icon}`)
 
-        await interaction.editReply({ embeds: [opening] })
+            await interaction.editReply({ embeds: [opening] })
 
-        if (!dbData) {
-            return inventory.create({
-                user_id: interaction.user.id
-            })
-        } else {
-            let keyName
-            if (skinsData[caseName]['name'] == 'Operation Breakout Weapon Case') {
-                keyName = 'Operation Breakout Case Key'
+            if (!dbData) {
+                return inventory.create({
+                    user_id: interaction.user.id
+                })
             } else {
-                keyName = `${skinsData[caseName]['name']} Key`
+                let keyName
+                if (skinsData[caseName]['name'] == 'Operation Breakout Weapon Case') {
+                    keyName = 'Operation Breakout Case Key'
+                } else {
+                    keyName = `${skinsData[caseName]['name']} Key`
+                }
+
+                const itemIndex = await dbData.inventory.findIndex((item) => item.skin === keyName);
+                const item = dbData.inventory[itemIndex];
+                const quantity = item.quantity;
+
+                if (quantity == 1) {
+                    await dbData.inventory.splice(itemIndex, 1);
+                } else {
+                    item.quantity -= 1;
+                }
+
+                await dbData.save();
             }
 
-            const itemIndex = await dbData.inventory.findIndex((item) => item.skin === keyName);
-            const item = dbData.inventory[itemIndex];
-            const quantity = item.quantity;
+            await giveXp(user, userData, interaction.channelId, client)
+            await userData.save()
 
-            if (quantity == 1) {
-                await dbData.inventory.splice(itemIndex, 1);
+            let probabilities = {
+                "Mil-spec": 0.7992327,
+                "Restricted": 0.1598465,
+                "Classified": 0.0319693,
+                "Covert": 0.0063939,
+                "Special Item": 0.0025575
+            };
+
+            if (userData.devMode) {
+                probabilities = {
+                    "Mil-spec": 0,
+                    "Restricted": 0,
+                    "Classified": 0,
+                    "Covert": 0,
+                    "Special Item": 100
+                }
+            }
+
+            let obtainedSkin = getRandomSkin(Skins, probabilities)
+            let finalSkin = `${obtainedSkin.skin} (${obtainedSkin.condition})`
+
+            if (obtainedSkin.condition == "vanilla") {
+                finalSkin = `${obtainedSkin.skin}`
+            }
+
+            let itemInfo = await getItem(finalSkin)
+            let skinPrice = itemInfo.buff163.starting_at.price
+            let skinIcon = obtainedSkin.icon
+
+            const embed = new EmbedBuilder()
+                .setTitle(`${skinsData[caseName].name}`)
+                .setColor(obtainedSkin.rarity === 'Mil-spec' ? 0x4b69ff :
+                    obtainedSkin.rarity === 'Restricted' ? 0x8847ff :
+                        obtainedSkin.rarity === 'Classified' ? 0x8847ff :
+                            obtainedSkin.rarity === 'Covert' ? 0xeb4b4b :
+                                obtainedSkin.rarity === 'Special Item' ? 0xffd700 : 'Black')
+                .setFooter({ text: `Automatically sold if you don't select keep in 10 seconds` })
+                .setDescription(`You got **${finalSkin}**\n Price: $${skinPrice}`)
+
+            // if (skinPrice.median) {
+            //     embed.setDescription(`You got **${finalSkin}**\n Price: $${skinPrice.median}`)
+            // } else if (skinPrice.average) {
+            //     embed.setDescription(`You got **${finalSkin}**\n Price: $${skinPrice.average}`)
+            // } else {
+            //     embed.setDescription(`You got **${finalSkin}**\n Price: $${skinPrice}`)
+            // }
+
+            if (userData.devMode) embed.setFooter({ text: `Automatically sold if you don't select keep in 10 seconds\n\n⚙️ Testing Mode - Chances might be different than normal` })
+
+            if (skinIcon) {
+                embed.setThumbnail(`${skinIcon}`)
             } else {
-                item.quantity -= 1;
+                embed.setThumbnail(`https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png`)
             }
 
-            await dbData.save();
-        }
+            const keep = new ButtonBuilder()
+                .setCustomId('keep')
+                .setLabel('Keep')
+                .setStyle(ButtonStyle.Primary)
 
-        await giveXp(user, userData, interaction.channelId, client)
-        await userData.save()
+            const sell = new ButtonBuilder()
+                .setCustomId('sell')
+                .setLabel('Sell')
+                .setStyle(ButtonStyle.Success)
 
-        let probabilities = {
-            "Mil-spec": 0.7992327,
-            "Restricted": 0.1598465,
-            "Classified": 0.0319693,
-            "Covert": 0.0063939,
-            "Special Item": 0.0025575
-        };
+            const row = new ActionRowBuilder()
+                .addComponents(keep, sell)
 
-        if (userData.devMode) {
-            probabilities = {
-                "Mil-spec": 0,
-                "Restricted": 0,
-                "Classified": 0,
-                "Covert": 0,
-                "Special Item": 100
-            }
-        }
-
-        let obtainedSkin = getRandomSkin(Skins, probabilities)
-        let finalSkin = `${obtainedSkin.skin} (${obtainedSkin.condition})`
-
-        if (obtainedSkin.condition == "vanilla") {
-            finalSkin = `${obtainedSkin.skin}`
-        }
-
-        let itemInfo = await getItem(finalSkin)
-        let skinPrice = itemInfo.buff163.starting_at.price
-        let skinIcon = obtainedSkin.icon
-
-        const embed = new EmbedBuilder()
-            .setTitle(`${skinsData[caseName].name}`)
-            .setColor(obtainedSkin.rarity === 'Mil-spec' ? 0x4b69ff :
-                obtainedSkin.rarity === 'Restricted' ? 0x8847ff :
-                    obtainedSkin.rarity === 'Classified' ? 0x8847ff :
-                        obtainedSkin.rarity === 'Covert' ? 0xeb4b4b :
-                            obtainedSkin.rarity === 'Special Item' ? 0xffd700 : 'Black')
-            .setFooter({ text: `Automatically sold if you don't select keep in 10 seconds` })
-            .setDescription(`You got **${finalSkin}**\n Price: $${skinPrice}`)
-
-        // if (skinPrice.median) {
-        //     embed.setDescription(`You got **${finalSkin}**\n Price: $${skinPrice.median}`)
-        // } else if (skinPrice.average) {
-        //     embed.setDescription(`You got **${finalSkin}**\n Price: $${skinPrice.average}`)
-        // } else {
-        //     embed.setDescription(`You got **${finalSkin}**\n Price: $${skinPrice}`)
-        // }
-
-        if (userData.devMode) embed.setFooter({ text: `Automatically sold if you don't select keep in 10 seconds\n\n⚙️ Testing Mode - Chances might be different than normal` })
-
-        if (skinIcon) {
-            embed.setThumbnail(`${skinIcon}`)
-        } else {
-            embed.setThumbnail(`https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png`)
-        }
-
-        const keep = new ButtonBuilder()
-            .setCustomId('keep')
-            .setLabel('Keep')
-            .setStyle(ButtonStyle.Primary)
-
-        const sell = new ButtonBuilder()
-            .setCustomId('sell')
-            .setLabel('Sell')
-            .setStyle(ButtonStyle.Success)
-
-        const row = new ActionRowBuilder()
-            .addComponents(keep, sell)
-
-        const setTimeoutPromise = (delay) => {
-            return new Promise((resolve) => {
-                setTimeout(resolve, delay);
-            });
-        };
-
-        const collectorFilter = i => i.user.id === interaction.user.id;
-        setTimeout(async () => {
-            const response = await interaction.editReply({ embeds: [embed], components: [row] })
-
-            try {
-                const confirmation = await response.awaitMessageComponent({
-                    filter: collectorFilter,
-                    time: 12_000,
+            const setTimeoutPromise = (delay) => {
+                return new Promise((resolve) => {
+                    setTimeout(resolve, delay);
                 });
+            };
 
-                if (confirmation.customId === 'keep') {
-                    if (!dbData) {
-                        return inventory.create({
-                            user_id: interaction.user.id
-                        })
-                    } else {
-                        const existingItem = dbData.inventory.find((i) => i.skin === finalSkin);
-                        if (existingItem) {
-                            existingItem.quantity += 1
+            const collectorFilter = i => i.user.id === interaction.user.id;
+            setTimeout(async () => {
+                const response = await interaction.editReply({ embeds: [embed], components: [row] })
+
+                try {
+                    const confirmation = await response.awaitMessageComponent({
+                        filter: collectorFilter,
+                        time: 12_000,
+                    });
+
+                    if (confirmation.customId === 'keep') {
+                        if (!dbData) {
+                            return inventory.create({
+                                user_id: interaction.user.id
+                            })
                         } else {
-                            await dbData.inventory.push({ skin: finalSkin, quantity: 1, icon: skinIcon})
+                            const existingItem = dbData.inventory.find((i) => i.skin === finalSkin);
+                            if (existingItem) {
+                                existingItem.quantity += 1
+                            } else {
+                                await dbData.inventory.push({ skin: finalSkin, quantity: 1, icon: skinIcon })
+                            }
+                            await dbData.save()
+                            await confirmation.update({
+                                content: `Kept`,
+                                components: [],
+                            });
                         }
-                        await dbData.save()
-                        await confirmation.update({
-                            content: `Kept`,
-                            components: [],
-                        });
+                    } else if (confirmation.customId === 'sell') {
+                        let tempPrice = skinPrice
+                        let fixedPrice = parseFloat(tempPrice)
+
+                        userData.balance += fixedPrice
+                        await userData.save()
+
+                        await confirmation.update({ content: 'Sold', components: [] });
                     }
-                } else if (confirmation.customId === 'sell') {
+                } catch (err) {
                     let tempPrice = skinPrice
                     let fixedPrice = parseFloat(tempPrice)
 
                     userData.balance += fixedPrice
                     await userData.save()
-
-                    await confirmation.update({ content: 'Sold', components: [] });
+                    await interaction.editReply({ content: 'Automatically sold', components: [] });
                 }
-            } catch (err) {
-                let tempPrice = skinPrice
-                let fixedPrice = parseFloat(tempPrice)
-
-                userData.balance += fixedPrice
-                await userData.save()
-                await interaction.editReply({ content: 'Automatically sold', components: [] });
-            }
-        }, 1150)
+            }, 1150)
+        }
     }
 }
 
